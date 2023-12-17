4ddedfcf049cb53242536fcdedf075caf7904967
Dwayne <dwaynereinaldy22@gmail.com>
2023-12-17 18:10:49 +0800

    adding calorie per day done

--- backend/back.js
+++ backend/back.js
@@ -3,95 +3,81 @@
 
 // To connect with your mongoDB database
 
-const express  = require("express");
-const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
+const express = require("express");
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const app = express();
 
-const secretKey = '23498hq3e413f40';
+const secretKey = "23498hq3e413f40";
 
 app.use(express.json());
-mongoose.connect("mongodb+srv://dwayne_reinaldy:mongodb123@cluster0.qufy1vp.mongodb.net/?retryWrites=true&w=majority", {
-	dbName: "FitnessWeb",
-	useNewUrlParser: true,
-	useUnifiedTopology: true
-});
+mongoose.connect(
+  "mongodb+srv://dwayne_reinaldy:mongodb123@cluster0.qufy1vp.mongodb.net/?retryWrites=true&w=majority",
+  {
+    dbName: "FitnessWeb",
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
 
 const db = mongoose.connection;
-db.on("error", console.error.bind(console,"connection failed: "));
-db.once("open",function(){
-	console.log("Connected to database successfully");
+db.on("error", console.error.bind(console, "connection failed: "));
+db.once("open", function () {
+  console.log("Connected to database successfully");
 });
 
-const verifyToken = (req, res, next) => {
-	const token = req.header('Authorization');
-  
-	if (!token) {
-	  return res.status(401).json({ message: 'Unauthorized' });
-	}
-  
-	jwt.verify(token, secretKey, (err, decoded) => {
-	  if (err) {
-		return res.status(401).json({ message: 'Invalid token' });
-	  }
-  
-	  req.userId = decoded.userId;
-	  next();
-	});
-  };
 const UserSchema = new mongoose.Schema({
-	name: {
-		type: String,
-		required: true,
-	},
-	height: {
-		type: Number,
-	},
-	weight: {
-		type: Number,
-	},
-	email: {
-		type: String,
-		required: true,
-		unique: true,
-	},
-	pass:{
-		type: String,
-		required: true,
-	},
+  name: {
+    type: String,
+    required: true,
+  },
+  height: {
+    type: Number,
+  },
+  weight: {
+    type: Number,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  pass: {
+    type: String,
+    required: true,
+  },
 });
 
 const calorieEntrySchema = new mongoose.Schema({
-	user: {
-	  type: mongoose.Schema.Types.ObjectId,
-	  ref: 'User',
-	  required: true,
-	},
-	entries: [
-	  {
-		date: {
-		  type: Date,
-		  default: Date.now,
-		},
-		food: {
-		  type: String,
-		  required: true,
-		},
-		totalCalories: {
-		  type: Number,
-		  required: true,
-		},
-	  },
-	],
-  });
-const User = mongoose.model('User', UserSchema);
-const CalorieEntry = mongoose.model('CalorieEntry', calorieEntrySchema);
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  entries: [
+    {
+      date: {
+        type: Date,
+        default: Date.now,
+      },
+      food: {
+        type: String,
+        required: true,
+      },
+      totalCalories: {
+        type: Number,
+        required: true,
+      },
+    },
+  ],
+});
+const User = mongoose.model("User", UserSchema);
+const CalorieEntry = mongoose.model("CalorieEntry", calorieEntrySchema);
 module.exports = { User, CalorieEntry };
 
 User.createIndexes();
 CalorieEntry.createIndexes();
-
 
 const cors = require("cors");
 const { Int32 } = require("mongodb");
@@ -99,50 +85,54 @@ console.log("App listen at port 5000");
 app.use(express.json());
 app.use(cors());
 app.get("/", (req, resp) => {
-
-	resp.send("App is Working");
+  resp.send("App is Working");
 });
 
-app.get('/users/find-user/:userId', async (req, res) => {
-	try {
-	  const userId = req.params.userId;
-	  console.log(userId)
-	  const user = await User.findById(userId);
-  
-	  if (!user) {
-		return res.status(404).json({ message: 'User not found' });
-	  }
-  
-	  res.json({ name: user.name, email: user.email, height: user.height, weight: user.weight });
-	} catch (error) {
-	  console.error('Error finding user:', error);
-	  res.status(500).json({ message: 'Internal Server Error' });
-	}
-  });
+app.get("/users/find-user/:userId", async (req, res) => {
+  try {
+    const userId = req.params.userId;
+    console.log(userId);
+    const user = await User.findById(userId);
 
-app.get('/users/total-calories/:userId/:dateCal', async (req, res) => {
-	try {
-	  const userId = req.params.userId;
-	  const dateCal = req.params.dateCal;
-	  const user = await CalorieEntry.find({
-		user: userId,
-		'entries.date': { $eq: new Date(dateCal) }
-	  });
-	  console.log(user)
-	  if (!user) {
-		return res.status(404).json({ message: 'User not found' });
-	  }
-	  const responseData = user.map(user => ({
-		userid: user.user,
-		entries: user.entries,
-	  }));
-	  
-	  res.json(responseData);
-	} catch (error) {
-	  console.error('Error finding user:', error);
-	  res.status(500).json({ message: 'Internal Server Error' });
-	}
-  });
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    res.json({
+      name: user.name,
+      email: user.email,
+      height: user.height,
+      weight: user.weight,
+    });
+  } catch (error) {
+    console.error("Error finding user:", error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+});
+
+app.get("/users/total-calories/:userId/:dateCal", async (req, res) => {
+  try {
+    const userId = req.params.userId;
+    const dateCal = req.params.dateCal;
+    const user = await CalorieEntry.find({
+      user: userId,
+      "entries.date": { $eq: new Date(dateCal) },
+    });
+    console.log(user);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    const responseData = user.map((user) => ({
+      userid: user.user,
+      entries: user.entries,
+    }));
+
+    res.json(responseData);
+  } catch (error) {
+    console.error("Error finding user:", error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+});
 
 // app.get('/users/total-calories/:userId/:dateCal', async (req, res) => {
 // 	try {
@@ -154,14 +144,14 @@ app.get('/users/total-calories/:userId/:dateCal', async (req, res) => {
 // 		user: userId,
 // 		'entries.date': { $eq: dateCal }, // Filter by user and date
 // 	  });
-  
+
 // 	  if (!entries.length) {
 // 		return res.status(404).json({ message: 'No entries found for that date' });
 // 	  }
-  
+
 // 	  res.json({
 // 		userid: userId,
-// 		entries : entries, 
+// 		entries : entries,
 // 	  });
 // 	} catch (error) {
 // 	  console.error('Error finding user:', error);
@@ -170,84 +160,84 @@ app.get('/users/total-calories/:userId/:dateCal', async (req, res) => {
 //   });
 
 app.post("/register", async (req, resp) => {
-	try {
-	  const { name, height, weight, email, pass } = req.body;
-  
-	  if (!pass) {
-		return resp.status(400).send("Password is required");
-	  }
-	  const hashedPassword = await bcrypt.hash(pass, 10);
+  try {
+    const { name, height, weight, email, pass } = req.body;
 
-	  const user = new User({
-		name,
-		height,
-		weight,
-		email,
-		pass: hashedPassword,
-	  });
-  
-	  // Save the user to the database
-	  let result = await user.save();
-	  result = result.toObject();
-  
-	  if (result) {
-		// Do not send the request body in the response
-		delete result.password;
-  
-		resp.send(result);
-		console.log(result);
-	  } else {
-		console.log("User already registered");
-	  }
-  
-	} catch (e) {
-	  console.error(e);
-	  resp.status(500).send("Something Went Wrong");
-	}
-  });
-  
+    if (!pass) {
+      return resp.status(400).send("Password is required");
+    }
+    const hashedPassword = await bcrypt.hash(pass, 10);
+
+    const user = new User({
+      name,
+      height,
+      weight,
+      email,
+      pass: hashedPassword,
+    });
+
+    // Save the user to the database
+    let result = await user.save();
+    result = result.toObject();
+
+    if (result) {
+      // Do not send the request body in the response
+      delete result.password;
+
+      resp.send(result);
+      console.log(result);
+    } else {
+      console.log("User already registered");
+    }
+  } catch (e) {
+    console.error(e);
+    resp.status(500).send("Something Went Wrong");
+  }
+});
 
 app.post("/login", async (req, res) => {
-	try {
-	  const { emailLog, passLog } = req.body;
-  
-	  const user = await User.findOne({ email: emailLog });
-	  if (user && (await bcrypt.compare(passLog, user.pass))) {
-		const token = jwt.sign({ userId: user._id }, secretKey, { expiresIn: '1h' });
-		res.json({ token, user: { _id: user._id, name: user.name, email: user.email } });
-	  } else {
-		res.status(401).json({ message: 'Invalid credentials' });
-	  }
-	} catch (error) {
-	  console.error(error);
-	  res.status(500).json({ message: 'Something went wrong' });
-	}
-  });
+  try {
+    const { emailLog, passLog } = req.body;
 
+    const user = await User.findOne({ email: emailLog });
+    if (user && (await bcrypt.compare(passLog, user.pass))) {
+      const token = jwt.sign({ userId: user._id }, secretKey, {
+        expiresIn: "1h",
+      });
+      res.json({
+        token,
+        user: { _id: user._id, name: user.name, email: user.email },
+      });
+    } else {
+      res.status(401).json({ message: "Invalid credentials" });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Something went wrong" });
+  }
+});
 
-  
+app.post("/calorie-entry", async (req, res) => {
+  try {
+    const { user, entries } = req.body;
 
-app.post('/calorie-entry', async (req, res) => {
-	try {
-	  const { user, entries } = req.body;
-  
-	const calorieEntry = new CalorieEntry({
-		user,
-		entries,
-	});
-  
-	const result = await calorieEntry.save();
-  
-	if (result) {
-		res.json(result);
-		console.log(result);
-	} else {
-		  console.log('Failed to save calorie entry');
-		  res.status(500).json({ error: 'Failed to save calorie entry' });
-	}
-	} catch (e) {
-	  console.error(e);
-	  res.status(500).json({ message: 'Something Went Wrong' });
-	}
+    const calorieEntry = new CalorieEntry({
+      user,
+      entries,
+    });
+
+    const result = await calorieEntry.save();
+
+    if (result) {
+      res.json(result);
+      console.log(result);
+    } else {
+      console.log("Failed to save calorie entry");
+      res.status(500).json({ error: "Failed to save calorie entry" });
+    }
+  } catch (e) {
+    console.error(e);
+    res.status(500).json({ message: "Something Went Wrong" });
+  }
 });
 app.listen(5000);
--- src/HomePage.js
+++ src/HomePage.js
@@ -1,24 +1,27 @@
 // UserProfile.js (or any relevant component)
-import React, { useState, useEffect } from 'react';
-import moment from 'moment';
+import React, { useState, useEffect } from "react";
+import moment from "moment";
 
 const HomePage = () => {
   const [user, setUser] = useState(null);
   const [userCal, setUserCal] = useState(null);
   const [error, setError] = useState(null);
-  const [date, setDate] = useState('');
-  const [food, setFood] = useState('');
-  const [totalCalories, setTotalCalories] = useState('');
-  const [dateCal, setDateCal] = useState('');
-  const [showCal, setShowCal] = useState('');
+  const [date, setDate] = useState("");
+  const [food, setFood] = useState("");
+  const [totalCalories, setTotalCalories] = useState("");
+  const [dateCal, setDateCal] = useState("");
+  const [showCal, setShowCal] = useState("");
+  const [calDay, setCalDay] = useState("");
 
   useEffect(() => {
-    const userId = localStorage.getItem('user_id');
-    const dateStorage = localStorage.getItem('date');
+    const userId = localStorage.getItem("user_id");
+    const dateStorage = localStorage.getItem("date");
     const fetchUser = async () => {
       try {
-        const response = await fetch('http://localhost:5000/users/find-user/' + userId);
-        
+        const response = await fetch(
+          "http://localhost:5000/users/find-user/" + userId
+        );
+
         if (response.ok) {
           const userData = await response.json();
           setUser(userData);
@@ -29,8 +32,8 @@ const HomePage = () => {
           setUser(null);
         }
       } catch (error) {
-        console.error('Error fetching user:', error);
-        setError('Something went wrong');
+        console.error("Error fetching user:", error);
+        setError("Something went wrong");
         setUser(null);
       }
     };
@@ -38,11 +41,11 @@ const HomePage = () => {
     if (userId) {
       fetchUser();
     }
-  }, []); 
+  }, []);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const userId = localStorage.getItem('user_id');
+    const userId = localStorage.getItem("user_id");
     const dataToSend = {
       user: userId,
       entries: [
@@ -56,58 +59,67 @@ const HomePage = () => {
     console.log(dataToSend);
 
     try {
-      const response = await fetch('http://localhost:5000/calorie-entry', {
-        method: 'POST',
+      const response = await fetch("http://localhost:5000/calorie-entry", {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
         body: JSON.stringify(dataToSend),
       });
 
       if (response.ok) {
         const responseData = await response.json();
-        console.log('Calorie entry saved:', responseData);
-        setDate('');
-        setFood('');
-        setTotalCalories('');
+        console.log("Calorie entry saved:", responseData);
+        setDate("");
+        setFood("");
+        setTotalCalories("");
       } else {
-        console.error('Failed to save calorie entry');
+        console.error("Failed to save calorie entry");
       }
     } catch (error) {
-      console.error('Error:', error);
+      console.error("Error:", error);
     }
   };
 
   const handleCalDay = async (e) => {
-    localStorage.setItem('date', dateCal);
+    localStorage.setItem("date", dateCal);
 
     e.preventDefault();
-    const userId = localStorage.getItem('user_id');
-    console.log(dateCal)
+    const userId = localStorage.getItem("user_id");
+    console.log(dateCal);
     try {
-      const response = await fetch(`http://localhost:5000/users/total-calories/${userId}/${dateCal}`);
+      const response = await fetch(
+        `http://localhost:5000/users/total-calories/${userId}/${dateCal}`
+      );
 
       if (!response.ok) {
         throw new Error(`HTTP error! Status: ${response.status}`);
       }
 
-      const contentType = response.headers.get('content-type');
-      if (!contentType || !contentType.includes('application/json')) {
-        throw new Error('Invalid JSON response');
+      const contentType = response.headers.get("content-type");
+      if (!contentType || !contentType.includes("application/json")) {
+        throw new Error("Invalid JSON response");
       }
-
+      let totalCaloriesDay = 0;
       const userData = await response.json();
+      userData.forEach((user) => {
+        user.entries.forEach((entry) => {
+          totalCaloriesDay += entry.totalCalories;
+        });
+      });
+      
+      console.log("Total Calories for All Users: ", totalCaloriesDay);
       setUserCal(userData);
+      setCalDay(totalCaloriesDay);
       setError(null);
     } catch (error) {
-      console.error('Error fetching user:', error.message);
-      setError('Something went wrong');
+      console.error("Error fetching user:", error.message);
+      setError("Something went wrong");
       setUserCal(null);
     } finally {
       setShowCal(!showCal);
     }
-  }
-  
+  };
 
   return (
     <div>
@@ -121,59 +133,72 @@ const HomePage = () => {
           {/* Display other user-specific data as needed */}
         </div>
       ) : (
-        <p>{error || 'Loading...'}</p>
+        <p>{error || "Loading..."}</p>
       )}
-      <h1>
-        Food
-      </h1>
+      <h1>Food</h1>
       <form onSubmit={handleSubmit}>
-      <label>
-        Date:
-        <input type="date" value={date} onChange={(e) => setDate(e.target.value)} />
-      </label>
+        <label>
+          Date:
+          <input
+            type="date"
+            value={date}
+            onChange={(e) => setDate(e.target.value)}
+          />
+        </label>
+        <br />
+        <label>
+          Food:
+          <input
+            type="text"
+            value={food}
+            onChange={(e) => setFood(e.target.value)}
+          />
+        </label>
+        <br />
+        <label>
+          Total Calories:
+          <input
+            type="number"
+            value={totalCalories}
+            onChange={(e) => setTotalCalories(e.target.value)}
+          />
+        </label>
+        <br />
+        <button type="submit">Submit</button>
+      </form>
       <br />
-      <label>
-        Food:
-        <input type="text" value={food} onChange={(e) => setFood(e.target.value)} />
-      </label>
-      <br />
-      <label>
-        Total Calories:
-        <input
-          type="number"
-          value={totalCalories}
-          onChange={(e) => setTotalCalories(e.target.value)}
-        />
-      </label>
-      <br />
-      <button type="submit">Submit</button>
-    </form>
-    <br/>
-    <form onSubmit={handleCalDay}>
-      <label>
-        Date:
-        <input type="date" value={dateCal} onChange={(e) => setDateCal(e.target.value)} />
-      </label>
-      <button type="submit">Submit</button>
-    </form>
-    {userCal? (
-  userCal.map((user, userIndex) => (
-    <div key={userIndex}>
-      <p>User ID: {user.userid}</p>
-      <ul>
-        {user.entries.map((entry, entryIndex) => (
-          <li key={entryIndex}>
-            <p>Date: {entry.date}</p>
-            <p>Food: {entry.food}</p>
-            <p>Calories: {entry.totalCalories}</p>
-          </li>
-        ))}
-      </ul>
-    </div>
-  ))
-) : (
-  <p>{error || 'No entries on this date'}</p>
-)}
+      <form onSubmit={handleCalDay}>
+        <label>
+          Date:
+          <input
+            type="date"
+            value={dateCal}
+            onChange={(e) => setDateCal(e.target.value)}
+          />
+        </label>
+        <button type="submit">Submit</button>
+      </form>
+      {userCal ? (
+        userCal.map((user, userIndex) => (
+          <div key={userIndex}>
+            <p>User ID: {user.userid}</p>
+            <ul>
+              {user.entries.map((entry, entryIndex) => (
+                <li key={entryIndex}>
+                  <p>Date: {entry.date}</p>
+                  <p>Food: {entry.food}</p>
+                  <p>Calories: {entry.totalCalories}</p>
+                </li>
+              ))}
+            </ul>
+          </div>
+        ))
+      ) : (
+        <p>{error || "No entries on this date"}</p>
+      )}
+      <p>
+        Total Calories this day: {calDay}
+      </p>
     </div>
   );
 };
